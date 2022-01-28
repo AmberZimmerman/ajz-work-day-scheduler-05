54b2e0e316fd6484d906bb78368351ebd4ccaf29
Amber Zimmerman <ajz1221@gmail.com>
2022-01-28 09:57:38 -0700

    adding classes to each column and some outline styling

--- Develop/script.js
+++ Develop/script.js
@@ -16,3 +16,20 @@
 
 let today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
+
+$("#weekly-schedule").dayScheduleSelector({
+    days: [0, 1, 2, 3, 4, 5, 6],
+    startTime:'08:00',
+    endTime:'20:00',
+    interval: 30,
+    stringDays  : ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'],
+    template    :'<div class="day-schedule-selector">' + 
+    '<table class="schedule-table">' + 
+    '<head class="schedule-header"></head>' + '<body class="schedule-rows"></body>' + 
+    '</table>' + '<div>'
+ });
+    
+ $("#weekly-schedule").data('artsy.dayScheduleSelector').deserialize({
+      '0': [['09:30','11:00'], ['13:00','16:30']]
+    });
+    
