a9e62546479eb15027b9cf2d1622b1454849bfa9
Amber Zimmerman <ajz1221@gmail.com>
2022-01-30 10:21:04 -0700

    cleaning up some notes and starting process to color code hours of day in real time

--- Develop/script.js
+++ Develop/script.js
@@ -14,121 +14,127 @@
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
 
+// This presents the current time and day
 let today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
-const hourBlocks = [
+// This is the array that I'm going to use to change out tasks per hour
+let initialHourData = [
     {   id: 1,
         hour: 9,
         task: "",
-        save: false
     },
     {   id: 2,
         hour: 10,
-        task: "Eat cheese",
-        save: true
+        task: "",
     },
     {   id: 3,
         hour: 11,
-        task: "Clean cowgirl boots",
-        save: true
+        task: "",
     },
     {   id: 4,
         hour: 12,
-        task: "Tip my hat",
-        save: true
+        task: "",
     },
     {   id: 5,
         hour: 13,
-        task: "Get a new saddle",
-        save: true
+        task: "",
     },
     {   id: 6,
         hour: 14,
-        task: "Put on fringe vest",
-        save: true
+        task: "",
     },
     {   id: 7,
         hour: 15,
         task: "",
-        save: true
     },
     {   id: 8,
         hour: 16,
-        task: "Sing a sad song",
-        save: true
+        task: "",
     },
     {   id: 9,
         hour: 17,
-        task: "Dance",
-        save: true
+        task: "",
     }
 
 ]
 
-console.log('start loop')
-// 1. Define starting number - DONE
-// 2. Do a check first
-// 3. If it passes check, run code block {}
-// 4. Code Finishes
-// 5. Then do number increate
-// 6. Do check on index
-// 7. Run code
-// 8. Code finishes
-// 9. index++
-// 10. Does Check
-// 11. Doesn't pass check
-// 12. Exit for loop
-
-
-
-const el = document.getElementById('hour-blocks');
-
-for (let index = 0; index < hourBlocks.length; index++) {
-    const block = hourBlocks[index];
-    const hour = block.hour;
-    const hourEl = document.createElement('div');
-    hourEl.innerText = block.hour;
-    hourEl.classList.add('hours');
-    hourEl.classList.add('col-1');
-    
-    const task = block.task;
-    const taskEl = document.createElement('input');
-    taskEl.innerText = block.task;
-    taskEl.classList.add('task');
-    taskEl.classList.add('col-10');
-
-    const floppyDisk = document.createElement('button');
-    floppyDisk.classList.add('col-1');
-    floppyDisk.setAttribute('value', block.id);
-    taskEl.setAttribute('data-input-id', block.id);
-    floppyDisk.addEventListener("click", function () {
-        // User clicks on Button
-        // Then take the value from said button
-        const floppyDiskValue = floppyDisk.value;
-        // Loop through blocks array to find specific data
-        for (let index = 0; index < hourBlocks.length; index++) {
-            const currentBlock = hourBlocks[index];
-            const currentBlockId = currentBlock.id;
-            // If it finds matching ids from array and button, it does stuff!
-            if (floppyDiskValue == currentBlockId) {
-                console.log(currentBlock);
-                console.log('Floppy Value: ', floppyDiskValue);
-                // Create string parameter to select input
-                const value = taskEl.value;
-                currentBlock.task = value;
-                console.log(value);
-            }
-        }
-        
-    });
-    
-    const rowEl = document.createElement('div');
-    rowEl.classList.add('row');
-    el.appendChild(rowEl);
-    rowEl.appendChild(hourEl);
-    rowEl.appendChild(taskEl);
-    rowEl.appendChild(floppyDisk);
+// Checking to see if user has data in local storage.
+// If they don't have data, let's create some to start with from what's above.
+if (!localStorage.getItem('hourBlocks')) {
+    localStorage.setItem('hourBlocks', JSON.stringify(initialHourData))
 }
+
+// This entire function is what is going to allow the user to add data to the hour and save that specific task to local storage using a button to save
+function initiateApp() {
+    console.log('Initializing App');
+    const hourBlocks = JSON.parse(localStorage.getItem('hourBlocks'));
+    const el = document.getElementById('hour-blocks');
+    console.log(hourBlocks);
+
+    for (let index = 0; index < hourBlocks.length; index++) {
+        const block = hourBlocks[index];
+        const hour = block.hour;
+        const hourEl = document.createElement('div');
+        hourEl.innerText = hour;
+        hourEl.classList.add('hours');
+        hourEl.classList.add('col-1');
+        
+        const task = block.task;
+        const taskEl = document.createElement('input');
+        taskEl.value = block.task;
+        taskEl.classList.add('task');
+        taskEl.classList.add('col-10');
+    
+        const floppyDisk = document.createElement('button');
+        floppyDisk.classList.add('col-1');
+        floppyDisk.setAttribute('value', block.id);
+        taskEl.setAttribute('data-input-id', block.id);
+        floppyDisk.addEventListener("click", function () {
+            // User clicks on Button
+            // Then take the value from said button
+            const floppyDiskValue = floppyDisk.value;
+            // Loop through blocks array to find specific data
+            for (let index = 0; index < hourBlocks.length; index++) {
+                const currentBlock = hourBlocks[index];
+                const currentBlockId = currentBlock.id;
+                // If it finds matching ids from array and button, it does stuff!
+                if (floppyDiskValue == currentBlockId) {
+                    console.log(currentBlock);
+                    console.log('Floppy Value: ', floppyDiskValue);
+                    // Create string parameter to select input
+                    currentBlock.task = taskEl.value;
+                    localStorage.setItem("hourBlocks", JSON.stringify(hourBlocks));
+                }
+            }
+            
+        });
+        
+        const rowEl = document.createElement('div');
+        rowEl.classList.add('row');
+        el.appendChild(rowEl);
+        rowEl.appendChild(hourEl);
+        rowEl.appendChild(taskEl);
+        rowEl.appendChild(floppyDisk);
+    }
+}
+
+initiateApp();
+
+// Create a statement to determine if hours on app are before, after, or current to local time
+const date = moment()
+
+date.set({ 
+  hour:10,
+  minute:18,
+  second:0,
+  millisecond:0
+})
+
+console.log(date.set[3]);
+
+// if (initialHourData.hour === )
+
+
 
 
