7cdfee5cb865754a01b60e8c3ef3b77310998d1e
Amber Zimmerman <ajz1221@gmail.com>
2022-01-30 20:50:53 -0700

    added Dolly Parton image as input background for past events

--- Develop/script.js
+++ Develop/script.js
@@ -19,42 +19,52 @@ let today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
 // This is the array that I'm going to use to change out tasks per hour
+// <img src="./Assets/images/dolly-parton-01.jpg" />
 let initialHourData = [
     {   id: 1,
         hour: 9,
         task: "",
+        dolly: "01"
     },
     {   id: 2,
         hour: 10,
         task: "",
+        dolly: "02"
     },
     {   id: 3,
         hour: 11,
         task: "",
+        dolly: "03"
     },
     {   id: 4,
         hour: 12,
         task: "",
+        dolly: "04"
     },
     {   id: 5,
         hour: 13,
         task: "",
+        dolly: "05"
     },
     {   id: 6,
         hour: 14,
         task: "",
+        dolly: "06"
     },
     {   id: 7,
         hour: 15,
         task: "",
+        dolly: "07"
     },
     {   id: 8,
         hour: 16,
         task: "",
+        dolly: "08"
     },
     {   id: 9,
         hour: 17,
         task: "",
+        dolly: "09"
     }
 
 ]
@@ -62,7 +72,8 @@ let initialHourData = [
 // Checking to see if user has data in local storage.
 // If they don't have data, let's create some to start with from what's above.
 if (!localStorage.getItem('hourBlocks')) {
-    localStorage.setItem('hourBlocks', JSON.stringify(initialHourData))
+    const hourBlocks = JSON.stringify(initialHourData)
+    localStorage.setItem('hourBlocks', hourBlocks)
 }
 
 // This entire function is what is going to allow the user to add data to the hour and save that specific task to local storage using a button to save
@@ -82,11 +93,12 @@ function initiateApp() {
         
         const task = block.task;
         const taskEl = document.createElement('input');
-        taskEl.value = block.task;
+        taskEl.value = task;
         taskEl.classList.add('task');
         taskEl.classList.add('col-10');
     
         const floppyDisk = document.createElement('button');
+        floppyDisk.setAttribute('value', `<i class="far fa-save"></i>`);
         floppyDisk.classList.add('col-1');
         floppyDisk.setAttribute('value', block.id);
         taskEl.setAttribute('data-input-id', block.id);
@@ -110,30 +122,41 @@ function initiateApp() {
             
         });
         
+        // Creating a row that all the columns can append to then appending the columns
         const rowEl = document.createElement('div');
         rowEl.classList.add('row');
         el.appendChild(rowEl);
         rowEl.appendChild(hourEl);
         rowEl.appendChild(taskEl);
         rowEl.appendChild(floppyDisk);
+
+        // Create a statement to determine if hours on app are before, after, or current to local time
+        const currentTime = moment().format('H');
+        // can use typeof to tell me if it returns a string
+        console.log(typeof currentTime);
+        const realTimeNumber = parseInt(currentTime);
+        console.log(typeof realTimeNumber);
+
+        // Create a statement to change colors, grey for past, red for current, green for future
+        if (hour === realTimeNumber) {
+            taskEl.classList.add('task-present');
+        } else if (hour < realTimeNumber) {
+            // instead of the color grey, make all slots fill in a picture of Dolly Parton
+            taskEl.classList.add('dolly');
+            const grabDolly = block.dolly;
+            const dollyPicture = `./Assets/images/dolly-parton_${grabDolly}.jpg`;
+            taskEl.style.backgroundImage = `url(${dollyPicture})`;
+            console.log(dollyPicture);
+            // taskEl.classList.add('task-past');
+        } else if (hour > realTimeNumber) {
+            taskEl.classList.add('task-future');
+        }
     }
 }
 
 initiateApp();
 
-// Create a statement to determine if hours on app are before, after, or current to local time
-const date = moment()
 
-date.set({ 
-  hour:10,
-  minute:18,
-  second:0,
-  millisecond:0
-})
-
-console.log(date.set[3]);
-
-// if (initialHourData.hour === )
 
 
 
